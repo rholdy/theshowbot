9521e804c3acb769473248d3c2b1eac342d84388
Holdy <rholdy@gmail.com>
2021-05-13 09:14:27 -0600

    optimization

--- lib/api.rb
+++ lib/api.rb
@@ -24,8 +24,6 @@ class Api
           if item[:rarity] == "Diamond" or item[:rarity] == "Gold"
             @item_ids << item[:uuid]
             puts "added #{item[:type]} with a rarity of #{item[:rarity]}"
-          else
-            puts "crappy #{item[:rarity]} #{item[:type]} card skipped"
           end
         end
       }
@@ -44,8 +42,6 @@ class Api
           if item[:rarity] == "Diamond" or item[:rarity] == "Gold"
             @item_ids << item[:uuid]
             puts "added #{item[:type]} with a rarity of #{item[:rarity]}"
-          else
-            puts "crappy #{item[:rarity]} #{item[:type]} card skipped"
           end
         end
       }
@@ -55,27 +51,31 @@ class Api
     @item_ids.uniq.each do |id|
       response = RestClient.get(base_url + "/listing.json?uuid=#{id}")
       body = JSON.parse(response.body, symbolize_names: true)
+
+      tax = (body[:best_sell_price] * 0.10).round
+      delta = (body[:best_sell_price] - body[:best_buy_price])
+      profit = delta - tax > 0 ? delta - tax : 1
+      buy = body[:best_buy_price] > 0 ? body[:best_buy_price] : 1
       card = {
           name: body[:listing_name],
           sell_price: body[:best_sell_price],
-          buy_price: body[:best_buy_price],
-          tax: (body[:best_sell_price] * 0.10),
-          delta: (body[:best_sell_price] - body[:best_buy_price]),
-          profit: ((body[:best_sell_price] - body[:best_buy_price]) - (body[:best_sell_price] * 0.10)).round
+          buy_price: buy,
+          tax: tax,
+          delta: delta,
+          profit: profit,
+          prof_percent: ((profit.to_f / buy.to_f) * 100).round
       }
 
-      if card[:profit] > 1000
+      if card[:profit] > 1000 && buy > 1
         cards << card
-        puts "added card with a profit of #{card[:profit]}"
-      else
-        puts "skipped card with #{card[:profit]}"
+        puts "added #{card[:name]} with a profit of #{card[:profit]}"
       end
     end
 
     sorted_cards = cards.sort { |a,b| b[:profit] <=> a[:profit] }
 
     sorted_cards.each do |card|
-      puts "#{card[:name]} -- PROFIT: #{card[:profit]} -- BUY: #{card[:buy_price]} -- SELL: #{card[:sell_price]}"
+      puts "#{card[:name]} -- PROFIT: #{card[:profit]} -- PERCENT: #{card[:prof_percent]} -- BUY: #{card[:buy_price]} -- SELL: #{card[:sell_price]}"
       puts "----------------------------------------------------------------------------------------------------"
     end
 
